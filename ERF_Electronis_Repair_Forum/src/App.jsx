aadc76d2cfad5342d36c9fe29ebf2bfbf80f205f
yoonjookim <yjblossom@gmail.com>
2024-05-06 15:06:23 -0400

    Add dynamic function, 'Navigate Topic' on my prev-static condition.

--- ERF_Electronis_Repair_Forum/src/App.jsx
+++ ERF_Electronis_Repair_Forum/src/App.jsx
@@ -5,34 +5,97 @@ import ReplyForm from './components/ReplyForm'
 import ReplyList from './components/ReplyList'
 import ReplyListItem from './components/ReplyListItem'
 
-import { t1 } from '../../data/topics'
+import { topicsObj } from '../../data/topics'
 import { usersObj } from '../../data/users'
 import { generatePostHeaderInfo } from './helpers/postHelpers'
-
-//  reply contains {userId:1, content:""}
-const singularReply = t1.replies[2];
-const allTheReplies = t1.replies;
+import { useState} from "react";
 
 
 function App() {
-  const fakeOnSubmit = reply => console.log(reply);
+  const [users, setUsers] = useState(usersObj);
+  const [topics, setTopics] = useState(topicsObj);
+  const [currentTopicId, setCurrentTopicId] = useState(1); //In topics.js data, const topicsObj = { 1: t1, 2: t2, 3: t3 }; the key starts with 1.
 
-  const postHeaderInfo = generatePostHeaderInfo(t1, usersObj);
+
+  //Navigating code is important JS question.
+  //"How do you go over the elements of an array,
+  //infinitely to the right & infinitely to the left without crashing?"
+  const navigateTopic = (isNext) => {   //Boolean
+    const availableIds = Object.keys(topics);
+    //[1, 2, 3];
+    const nextValueIndex =
+     (availableIds.indexOf(currentTopicId) + 1) % availableIds.length;
+    
+    const prevValueIndex = 
+      availableIds.indexOf(currentTopicId) - 1 === -1
+        ? availableIds.length - 1
+        : availableIds.indexOf(currentTopicId) - 1;
+
+    if (isNext) {
+      setCurrentTopicId(availableIds[nextValueIndex]);
+    } else {
+      setCurrentTopicId(availableIds[prevValueIndex]);
+    }
+  };
+
+
+
+
+  const topic = topics[currentTopicId];
+
+  const postHeaderInfo = generatePostHeaderInfo(topic, users);
   // const replyListItemInfo = generateReplyListItemInfo(singularReply, usersObj)
-   
-  // return (
-  //   <>
-  //     <Header />
-  //     <main>
-  //       <PostHeader />
-  //       <ReplyList />
-  //       <ReplyForm />
-  //     </main>
-       
-  //   </>
-  // );
 
-  return <Header />; 
+
+  const addReplyToTopic = (reply) => {
+
+    const updatedTopics = { ...topics };
+    const updatedTopic = { ...topic };
+
+    const replyObj = {
+      content: reply,
+      userId: Math.ceil(Math.random() * 39), //I have 39 profile_images
+    };
+
+    updatedTopic.replies = [ ...topic.replies, replyObj];
+
+    updatedTopics[currentTopicId] = updatedTopic;
+
+    setTopics(updatedTopics);
+  }
+
+
+  const addReplyToTopicId = (reply) => {
+    const updatedTopic = { ...topic };
+
+    const replyObj = {
+      content: reply,
+      userId: Math.ceil(Math.random() * 39),
+    };
+
+    updatedTopic.replies = [...topic.replies, replyObj];
+
+    setTopic(updatedTopic);
+  };
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section className="Navigator">
+          <button onClick={() => navigateTopic(false)}>Back</button>
+          <span>Currently looking at topic #{currentTopicId}</span>
+          <button onClick={() => navigateTopic(true)}>Next</button>
+        </section>
+        <PostHeader { ...postHeaderInfo}/>
+        <ReplyList replies={topic.replies} users={users}/>
+        <ReplyForm onSubmit={addReplyToTopic}/>
+      </main>
+       
+    </>
+  );
+
+  // return <Header />; 
   // return <PostHeader { ...postHeaderInfo} />;
   // return <ReplyForm onSubmit={fakeOnSubmit} />;
   // return <ReplyList replies={allTheReplies} users={usersObj} />;
