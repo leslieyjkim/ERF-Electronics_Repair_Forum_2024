f1dc8d5ee60156c40f88610254e8dacdb650c00b
yoonjookim <yjblossom@gmail.com>
2024-05-06 12:19:36 -0400

    Debug typo, now works

--- ERF_Electronis_Repair_Forum/src/App.jsx
+++ ERF_Electronis_Repair_Forum/src/App.jsx
@@ -8,18 +8,26 @@ import ReplyListItem from './components/ReplyListItem'
 import { t1 } from '../../data/topics'
 import { usersObj } from '../../data/users'
 
-//  {userId:1, content:""}
+//  reply contains {userId:1, content:""}
 const singularReply = t1.replies[0]
 
+
+//we're gonna find the way to create 'user object'
 const generateReplyListItemInfo = (reply, users) => {   //This gonna take reply, users
-  const output = {}                  //this function will create 'output' object, 
+  const output = {}    //this function will create 'output' object, 
+  const user = users[reply.userId] 
+               
   output.content = reply.content     //And then in there, output's content will be reply's content
-  output.profileUrl = users[reply.userId].profile_url //And the output's profileUrl that's gonna be users at the position of the reply.userId, and I'm gonna use their profile_url (from users.js in data)
+  output.profileUrl = user.profile_url //And the output's profileUrl that's gonna be users at the position of the reply.userId, and I'm gonna use their profile_url (from users.js in data)
+  output.author = user.username
+
+  return output;
 }
 
 
 function App() {
 
+  const replyListItemInfo = generateReplyListItemInfo(singularReply, usersObj)
   // return (
   //   <>
   //     <Header />
@@ -32,7 +40,7 @@ function App() {
   //   </>
   // );
 
-  return <ReplyListItem />;
+  return <ReplyListItem { ...replyListItemInfo } />;
 }
 
 export default App
